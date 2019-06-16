ec959a2e345b6d30f6c0ca15ce17d4f8c51ef3d2
Serena Gibbons <serenalgibbons@gmail.com>
2019-06-16 15:21:07 -0400

    Implement handleButtonInput method to display button input

--- src/calculator/CalculatorController.java
+++ src/calculator/CalculatorController.java
@@ -1,5 +1,21 @@
 package calculator;
 
-public class CalculatorController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+public class CalculatorController {
+	
+	private String value;
+	
+    @FXML
+    private TextField displayField;
+
+    @FXML
+    void handleButtonInput(ActionEvent event) {
+    	value = ((Button)event.getSource()).getText();			// get text of button input
+        displayField.setText(displayField.getText() + value);	// display value entered
+    }
+	
 }
