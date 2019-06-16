7da1c0ded27178946b6b479a09ebf721430d6e4d
Serena Gibbons <serenalgibbons@gmail.com>
2019-06-16 10:15:49 -0400

    Implement CalculatorApp as driver class

--- src/calculator/CalculatorApp.java
+++ src/calculator/CalculatorApp.java
@@ -1,5 +1,27 @@
 package calculator;
 
-public class CalculatorApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalculatorApp extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("CalculatorView.fxml"));
+		
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("Calculator");
+		primaryStage.setScene(scene);
+		primaryStage.show();		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 
 }
