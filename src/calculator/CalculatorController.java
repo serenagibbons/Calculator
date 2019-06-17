3c0264621c636656839206aed560b40cb3c86be2
Serena Gibbons <serenalgibbons@gmail.com>
2019-06-17 10:17:06 -0400

    Add history field and format display

--- src/calculator/CalculatorController.java
+++ src/calculator/CalculatorController.java
@@ -1,18 +1,24 @@
 package calculator;
 
 import java.util.ArrayList;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class CalculatorController {
 	
 	private String value;
+	private String history = "";
+	private double result;
+	private ArrayList<Double> input;
 	
     @FXML
     private TextField displayField;
+    
+    @FXML
+    private Text historyField;
 
     @FXML
     private void handleButtonInput(ActionEvent event) {
@@ -22,8 +28,17 @@ public class CalculatorController {
     	case "C":
     		clear(event);
     		break;
+    	case "+/-":
     	case "+":
+    	case "-":
+    	case "*":
+    	case "/":
+    		history += (displayField.getText() + value);
+    		historyField.setText(history); // display input history
+    		break;
+    	case "=":
     	default: 
+    		displayField.clear();
     		displayField.setText(displayField.getText() + value); // display value entered
     		break;
     	}
@@ -33,6 +48,17 @@ public class CalculatorController {
     @FXML
     private void clear(ActionEvent event) {
     	displayField.clear();
+    	historyField.setText("");
+    }
+    
+    @FXML
+    private void addition(ActionEvent event) {
+    	input = new ArrayList<>();
+    	
+    	if (isNumeric(value)) {
+
+    	}
+    	
     }
     
     private boolean isNumeric(String s) {
